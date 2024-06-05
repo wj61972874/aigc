85f5d6c0d6474ae9e7936822e2345d87c2ba0b52
wj61972874 <m15205519363@163.com>
2024-06-05 18:03:28 +0800

    feat(粉丝来信): 页面

--- server/interfaces/index.ts
+++
@@ -1,4 +0,0 @@
-enum TextGenerteType {
-  "success",
-  "error",
-}
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,17 +2,17 @@ import request from "./request";
 
 //文本生成
 export function apiGeneratePoem(data: any): Promise<any> {
-  return request.post(`http://115.29.214.125:3000/api/aigc/textGenerate`, data);
-  // return request.post(`http://127.0.0.1:3000/api/aigc/textGenerate`, data);
+  // return request.post(`http://115.29.214.125:3000/api/aigc/textGenerate`, data);
+  return request.post(`http://127.0.0.1:3000/api/aigc/textGenerate`, data);
 }
 
 //内容审查
 
 export function apiDoContentCensor(data: any): Promise<any> {
-  return request.post(
-    `http://115.29.214.125:3000/api/aigc/contentCensor`,
-    data
-  );
+  // return request.post(
+  //   `http://115.29.214.125:3000/api/aigc/contentCensor`,
+  //   data
+  // );
   // console.log("apiDoContentCensor----", data);
-  // return request.post(`http://127.0.0.1:3000/api/aigc/contentCensor`, data);
+  return request.post(`http://127.0.0.1:3000/api/aigc/contentCensor`, data);
 }
